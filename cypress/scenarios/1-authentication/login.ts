5db5da685c85a928ab5753d02e970493ce43f793
Decha_L <dacha2543@gmail.com>
2025-08-29 17:02:48 +0700

    feat: blind test login and verify login

--- cypress/scenarios/1-authentication/login.ts
+++ cypress/scenarios/1-authentication/login.ts
@@ -1,15 +1,23 @@
+import 'cypress-xpath';
+
 export function login() {
 	context('Login Steps', () => {
 		it('Input Username', () => {
-			
+			const usernameLocator = cy.xpath(`//input[@name='username']`);
+
+			usernameLocator.type("Username")
 		});
 
 		it('Input Password', () => {
+			const passwordLocator = cy.xpath(`//input[@name='password' and @type='password']`);
 
+			passwordLocator.type("Password")
 		});
 
 		it('Submit Login', () => {
+			const loginButtonLocator = cy.xpath(`//input[@type='submit' and @value='Send Request']`)
 
+			loginButtonLocator.click()
 		});
 	});
 }
--- cypress/scenarios/1-authentication/verify.ts
+++ cypress/scenarios/1-authentication/verify.ts
@@ -1,11 +1,14 @@
 export function verifyAfterLogin() {
 	context('Verify After Login Steps', () => {
 		it('Verify Welcome Message', () => {
+			const welcomeMessageLocator = cy.xpath(`//span[@class='welcome message']`);
 
+			welcomeMessageLocator.should('contain.text', 'Welcome Mr.John');
 		});
 
 		it('Verify Dashboard URL', () => {
-
+			cy.url().should('include', '/dashboard');
+			cy.url().should('not.include', '/login');
 		});
 	});
 }
